fb24db098af98fabd07f73245c5c7b3efea1048e
Pau <pauescola12@gmail.com>
2024-04-04 16:05:57 +0200

    Hem fet els textos del hero banner responsive, falta revisar el contact form i posar els tres idiomes

--- src/components/ContactFormModal.js
+++ src/components/ContactFormModal.js
@@ -4,9 +4,9 @@ import ContactForm from './ContactForm';
 
 function ContactFormModal({ onClose }) {
     return (
-        <div className="fixed inset-0 bg-gray-600 bg-opacity-75 flex justify-center items-center z-50">
-            <div className="bg-white p-4 rounded-lg max-w-lg w-full">
-                <button onClick={onClose} className="float-right font-bold">X</button>
+        <div className="fixed inset-0 z-50 overflow-auto bg-black bg-opacity-50 flex justify-center items-center">
+            <div className="relative bg-white p-5 rounded-lg shadow-lg max-w-md w-full m-4">
+                <button onClick={onClose} className="absolute top-0 right-0 mt-4 mr-4 text-3xl font-bold text-black">&times;</button>
                 <ContactForm />
             </div>
         </div>
--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -12,12 +12,12 @@ function HeroBanner() {
         <div className="relative h-[400px] md:h-[500px] lg:h-[800px] w-full">
             <Carousel images={[falsaD, falsaU, falsaT]} size="absolute inset-0 w-full h-full" objectFit={"object-cover"} />
             <div className="absolute inset-0 flex flex-col justify-center items-center text-center text-white space-y-4 bg-black bg-opacity-60">
-                <h1 className="text-4xl md:text-5xl font-bold">Benvingut a SIM</h1>
-                <p className="text-lg md:text-xl max-w-5xl">Una experiència de 20 anys d'especialització en tot tipus de solucions prefabricades: temporals o definitives, en lloguer o venda, noves o d'ocasió, fan del <b>GRUP SIM</b> una resposta en la construcció mòbil i edificació modular.
+                <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold">Benvingut a SIM</h1>
+                <p className="text-sm sm:text-md md:text-lg lg:text-xl max-w-2xl">Una experiència de 20 anys d'especialització en tot tipus de solucions prefabricades: temporals o definitives, en lloguer o venda, noves o d'ocasió, fan del <b>GRUP SIM</b> una resposta en la construcció mòbil i edificació modular.
                     Un servei global, on s'inclou la possibilitat del servei de manteniment, transformació o trasllat de qualsevol prefabricat de la seva propietat.
                     A SIM li assessorem o desenvolupem productes a mida de les seves necessitats.</p>
                 <button
-                    className=" text-xl bg-primary hover:bg-blue-700 text-white font-bold py-4 px-6 rounded-full z-50"
+                    className="mt-8 md:mt-12 lg:mt-16 text-lg md:text-xl lg:text-2xl bg-primary hover:bg-blue-700 text-white font-bold py-4 px-6 rounded-full z-40"
                     onClick={() => setIsModalOpen(true)}
                 >
                     Demanar pressupost
